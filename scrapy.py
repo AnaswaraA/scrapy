7088f41785cc9fb6562fe6c9adb3a334918e6b4f
AnaswaraA <69682308+AnaswaraA@users.noreply.github.com>
2022-02-05 22:28:41 +0530

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -2,6 +2,6 @@ import scrapy
 class class ClotheSpider(scrapy.Spider):
     name = 'clothes'
     start_urls = [
-        '',
+        'https://www.carbon38.com/shop-all-activewear/tops',
     ]
     
