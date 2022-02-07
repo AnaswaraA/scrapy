696f483be96a23cff1b4f3e252ddf1814fb6435f
AnaswaraA <69682308+AnaswaraA@users.noreply.github.com>
2022-02-07 19:29:27 +0530

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -6,13 +6,22 @@ class CarbonSpider(scrapy.Spider):
         "https://carbon38.com/collections/tops/products/miramar-candice-crop"
     ]
 
-    def parse(self, response):
+      def parse(self, response):
         all_div_quotes = response.css('div.product-detail')
+        # breadcrumbs = all_div_quotes.css('.breadcrumbs::text').extract()
         product_name = response.css(".title ::text").extract_first()
+        image_url = response.css("rimage").extract()
         brand = response.css(".vendor ::text")[2].extract()
+        price = response.css(".current-price.theme-money::text").get()
+        reviews = response.css("okeReviews-reviewsWidget-emptyMessage > p").get()
+        breadcrumbs = response.css('.breadcrumbs-list >li.breadcrumbs-list__item >.breadcrumbs-list__link ::text').extract()
+
         yield {
             'product_name': product_name,
-            'brand': brand
-          
+            'image_url': image_url,
+            'brand': brand,
+            'breadcrumbs': breadcrumbs,
+            'price': price,
+            'reviews': reviews
         }
     
